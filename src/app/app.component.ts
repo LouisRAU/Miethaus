c24bfda2260d71e370f3aaa124a9b3307caf57c5
LouisRAU <louis.schulle@edu.r-au.ch>
2025-04-14 14:52:11 +0200

    Aufgabe 2 HousingLocationComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, HousingLocationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
